262fde2eb5734f43e6b049200877e45f2fb6ff1b
Yamaoki <d89284146236sv@gmail.com>
2022-11-06 13:35:44 +0300

    дз 2-1

---
+++ src/Human.java
@@ -0,0 +1,35 @@
+public class Human {
+    String name;
+    String town;
+    int yearOfBirth;
+    String job;
+
+    Human(String name, String town, int year, String job) {
+        if (year > 0) {
+            this.yearOfBirth = 2022 - year;
+        } else {
+            yearOfBirth = 0;
+        }
+        if (name == null) {
+            this.name = "Информация не указана";
+        } else {
+            this.name = name;
+        }
+        if (town == null) {
+            this.town = "Информация не указана";
+        } else {
+            this.town = town;
+        }
+        if (job == null) {
+            this.job = "Информация не указана";
+        } else {
+            this.job = job;
+        }
+    }
+    void humanHe() {
+        System.out.println("Привет! Меня зовут - " + name + ". Я из города - " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности - " + job + " Будем знакомы!");
+    }
+    void humanShe() {
+        System.out.println("Привет! Меня зовут - " + name + ". Я из города - " + town + ". Я родилась в " + yearOfBirth + " году. Я работаю на должности - " + job + " Будем знакомы!");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        Human Maxim = new Human("Максим", "Минск", 35, "бренд-менеджера.");
+        Maxim.humanHe();
+        Human Anya = new Human("Аня", "Москва", 29, "методиста образовательных программ.");
+        Anya.humanShe();
+        Human Katya = new Human("Катя", "Калининград", 28, "продакт-менеджера.");
+        Katya.humanShe();
+        Human Artem = new Human("Артем", "Москва", 27, "директора по развитию бизнеса.");
+        Artem.humanHe();
 
     }
 }
